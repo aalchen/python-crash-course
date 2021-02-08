720bdcbcf95e68ea461c4396e85eb2126a82affe
aalchen <43652006+aalchen@users.noreply.github.com>
2021-02-07 23:21:25 -0800

    Chapter 8 in-progress

---
+++ chapter8.py
@@ -0,0 +1,10 @@
+# Python Crash Course exercises
+# by Eric Matthes
+# Chapter 7 - User input & while loops
+
+
+def greet_user(username):
+    print("Hello, " + username.title() + "!")
+
+
+greet_user('jesse')
