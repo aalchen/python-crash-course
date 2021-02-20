a4f9a39bd4d08b42565ebd94b3ef6d73461c13eb
aalchen <43652006+aalchen@users.noreply.github.com>
2021-02-19 23:04:20 -0800

    Chapter 8 in progress

--- chapter8.py
+++ chapter8.py
@@ -1,10 +1,54 @@
 # Python Crash Course exercises
 # by Eric Matthes
-# Chapter 7 - User input & while loops
+# Chapter 8 - Functions
 
 
-def greet_user(username):
-    print("Hello, " + username.title() + "!")
+#8-1 - Message
+def display_message():
+    print("I'm learning about functions in this chapter!")
 
 
-greet_user('jesse')
+display_message()
+
+
+#8-2 - Favorite book
+def favorite_book(title):
+    print("One of my favourite books is " + title)
+
+
+favorite_book("Perilous Kinship")
+
+
+#8-3 - T-shirt
+def make_shirt(size, text):
+    print("Your shirt is size " + size.upper() + " and the message says " +
+          text)
+
+
+#Positional arguments
+make_shirt("m", "Bing Bang Bong")
+
+#Keyword arguments
+make_shirt(text="Ding Dang Dong", size="L")
+
+
+#8-4 - Large t-shirts
+def make_shirt(size="L", text="I love Python"):
+    print("Your shirt is size " + size.upper() + " and the message says " +
+          text)
+
+
+make_shirt()
+make_shirt("M")
+make_shirt(text="Hi")
+
+#8-5 - Cities
+
+
+def describe_city(city, country="Canada"):
+    print(city.title() + " is in " + country.title() + ".")
+
+
+describe_city("Toronto")
+describe_city("Vancouver")
+describe_city("Mannheim", "Germany")
